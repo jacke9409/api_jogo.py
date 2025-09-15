a3d5c8511555a63f5ff634c3e173b6e9a3b67b9b
Jackelyne <Jackelyneramosbrito@gmal.com>
2025-09-15 12:48:56 -0300

    decimo quarto commit

--- api.py
+++ api.py
@@ -1,8 +1,34 @@
-# # links do site oficial
-# site oficial mine: https://www.minecraft.net/pt-br 
-
-# site oficial roblox: https://www.roblox.com/ 
-
-# site oficial fortnite: https://www.fortnite.com
-
-# site oficial zelda: https://www.nintendo.com/pt-pt/Jogos/Portal-Nintendo/Portal-The-Legend-of-Zelda/Portal-The-Legend-of-Zelda-627606.html?srsltid=AfmBOorV19QeXSh_Q7EEyerJ-K0duhYJfxmMUTt_GmWFMhQ1uuLi7rKp 
+# # =========================
+# # Jogos/Animes disponíveis
+# # =========================
+# games = {
+#     "Minecraft": {
+#         "lancamento": "2009",
+#         "genero": "Mundo aberto / Criativo",
+#         "curiosidades": ["Criado por Markus 'Notch' Persson.", "Mundos praticamente infinitos via geração procedural.", "Um dos jogos mais vendidos da história."],
+#         "detalhes": "Exploração e construção em mundos gerados por algoritmo.",
+#         "imagem": "https://i.pinimg.com/736x/5f/f0/e9/5ff0e997a4a8ba449056ed679660f4cc.jpg",
+#         "cor": "#8CC63F",
+#         "link": " https://www.minecraft.net/pt-br "
+#     },
+#     "The Legend of Zelda: Breath of the Wild": buscar_anime("Zelda"),  # Aqui usamos a API
+#     "Fortnite": {
+#         "lancamento": "2017",
+#         "genero": "Battle Royale",
+#         "curiosidades": ["Eventos ao vivo dentro do jogo.", "Crossplay entre várias plataformas.", "Temporadas mudam mapa e gameplay."],
+#         "detalhes": "Batalhas de 100 jogadores, construção e eventos sazonais.",
+#         "imagem": "https://i.pinimg.com/736x/7e/e8/c4/7ee8c4361736ed806711ae99f7d6762c.jpg",
+#         "cor": "#4B8BF5",
+#         "link": "https://www.fortnite.com/"
+#     },
+#     "Roblox": {
+#         "lancamento": "2006",
+#         "genero": "Plataforma / UGC",
+#         "curiosidades": ["Usuários criam experiências e jogos.", "Economia interna com itens da comunidade.", "Muito usado para aprender lógica e game design."],
+#         "detalhes": "Plataforma UGC com inúmeras modalidades criadas pela comunidade.",
+#         "imagem": "https://i.pinimg.com/736x/a6/8c/0b/a68c0bc57047b77fa9c25cb0a9a0cebb.jpg",
+#         "cor": "#FF0000",
+#         "link": "https://www.roblox.com/ "
+#     }
+# }
+# links
